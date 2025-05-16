2518cdf4d0aed0977a4e1d237842e2efba135c40
Mukilathuruvan Kanibalan <83006036+mukilathuruvan@users.noreply.github.com>
2025-05-16 19:38:09 +0200

    Fixed ISP_02

--- SOLID/ISP_02.java
+++ SOLID/ISP_02.java
@@ -7,12 +7,17 @@ public class ISP_02 {
      * Again, some classes are more simple than other.
      * Fix this without violating the ISP (Interface Segregation Principle)!
      */
-    public static interface Worker {
+
+    public static interface Workable {
         void work();
+    }
+
+    public static interface Restable {
         void rest();        
     }
 
-    public static class Developer implements Worker {
+    // by separating interfaces, now we are not forced to implement methods which we don't need
+    public static class Developer implements Workable, Restable {
         @Override
         public void work() {
             System.out.println("Developer is coding ...");
@@ -24,25 +29,29 @@ public class ISP_02 {
         }
     }
 
-    public static class Robot implements Worker {
+    // Robot is not abled to rest since we implemented only Workable interface
+    public static class Robot implements Workable {
         @Override
         public void work() {
             System.out.println("Robot is working hard ...");
         }
 
-        @Override
-        public void rest() {
-            throw new UnsupportedOperationException("Robot does not need a break!");
-        }        
     }
 
-    public static void workingCycle(Worker worker) {
+    public static void workingCycleForDeveloper(Workable worker, Restable rester) {
         worker.work();
-        worker.rest();
+        rester.rest();
+    }
+
+    public static void workingCycleForRobot(Workable worker) {
+        worker.work();
     }
 
     public static void main(String[] args) {
-        workingCycle( new Developer() );
-        workingCycle( new Robot() );        
+        Developer developer=new Developer();
+        Robot robot =new Robot();
+
+        workingCycleForDeveloper(developer, developer);
+        workingCycleForRobot(robot);        
     }
 }
