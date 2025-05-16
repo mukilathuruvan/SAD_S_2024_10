5feb716325a3cc03ed149bef0a1545813010fbbe
Mukilathuruvan Kanibalan <83006036+mukilathuruvan@users.noreply.github.com>
2025-05-16 18:47:29 +0200

    Fixed DIP_01

--- SOLID/DIP_01.java
+++ SOLID/DIP_01.java
@@ -7,26 +7,37 @@ public class DIP_01 {
      * The Switch is violating the DIP (Dependency Inversion Principle).
      * Please fix this!
      */
+
+
+    // Creating an interface to abstract the operations
+    public interface Operatable{
+        void turnOn();
+        void turnOff();
+    }
     
-    public static class LightBulb {
+    // Implementing the interface in the LightBulb class (now concrete class is dependent on the interface)
+    public static class LightBulb implements Operatable{
+        @Override
         public void turnOn() {
             System.out.println("Light is ON!");
         }
+
+        @Override
         public void turnOff() {
             System.out.println("Light is OFF!");
         }
     }
 
     public static class Switch {
-        private LightBulb lightBulb;
+        // Dependency on the interface instead of concrete class
+        private Operatable operatable;
 
-        // this is "Dependency Injection" (composition style)
-        public Switch(LightBulb lightBulb) {
-            this.lightBulb = lightBulb;
+        public Switch(Operatable device) {
+            this.operatable = device;
         }
 
         public void operate() {
-            lightBulb.turnOn();
+            operatable.turnOn();
         }
     }
 
